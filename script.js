bf1383d73767e654b8ef62c12ad8857b5d5cab7e
unknown <bardhr97@gmail.com>
2025-01-16 10:35:17 -0800

    punova

---
+++ script.js
@@ -0,0 +1,34 @@
+// let frutat =["molla","dardha","bananja"];
+
+// console.log(frutat[2])
+// console.log(frutat[0] + " me pelqen shume")
+// console.log("mirjeta" + +false);
+// console.log(3 + +"2")
+// console.log("scantech" + + "scantech")
+
+// let frutat =["molla","dardha","bananja"];
+
+// console.log(frutat)
+// //push pop
+// //push - shton element ne fund te vargut
+// frutat.push("pjedhka");
+// console.log(frutat);
+// //pop - e largon elementin e fundit te vargut
+// frutat.pop();
+// frutat.pop();
+// frutat.pop();
+
+// console.log(frutat);
+// //shift unshift
+// //unshift - vendos nje element ne filim te vargut
+// frutat.unshift("rrushi");
+// frutat.unshift("ananasi");
+// //shift - largon nje element nga fillimi i vargut
+// frutat.shift();
+
+//krijimi i vargjeve
+let vargu1 = [];
+//krijimi i vargut duke ja tregu gjatsin
+let vargu2 = new Array(5);
+let vargu3 = new Array(1,2,3,4);
+console.log(vargu3)
